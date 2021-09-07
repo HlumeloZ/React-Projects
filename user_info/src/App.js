54ef3f3e7cfa089abac87e5863da9e570935c60f
Hlumelo <hlumelozondo27@gmail.com>
2021-09-07 07:28:47 +0200

    React_Projects

---
+++ user_info/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Products from './Products';
+import { Button } from 'react-bootstrap';
+import Rating from './Rating';
+
+
+class App extends Component {
+  render() {
+    const isValid = true;
+
+    return (
+      <div>
+        <Rating rating="1"/>
+        <Rating rating="2"/>
+        <Rating rating="3"/>
+        <Rating rating="4"/>
+        <Rating rating="5"/>
+      </div>
+    );
+  }
+}
+
+export default App;
